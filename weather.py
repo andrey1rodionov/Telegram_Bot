f75fe8b597440853391b4d026f3251c5019130fd
andrey1rodionov <45152808+andrey1rodionov@users.noreply.github.com>
2018-11-21 23:23:16 +0300

    Add stop

--- weather.py
+++ weather.py
@@ -49,7 +49,7 @@ def get_weather_for_specific_city(city):
                   "\n" + str(description_brief) + \
                   "\n" + str(result_code)
     else:
-        error_code = parsed_resp['message']
+        # error_code = parsed_resp['message']
         message = 'ОШИБКА'
 
     return message
