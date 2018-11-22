0f7bdd2c2045cad0cb11ef274a7e4700b345cb67
andrey1rodionov <45152808+andrey1rodionov@users.noreply.github.com>
2018-11-23 00:41:14 +0300

    Add news.py

---
+++ news.py
@@ -0,0 +1,15 @@
+import urllib.request
+
+import json
+
+api_endpoint = "https://newsapi.org/v2/top-headlines?sources=google-news-ru"
+
+apikey = "ee793d3a00ff404780cad47b8967013e"
+
+news_url = api_endpoint + "&apiKey=" + apikey
+
+response = urllib.request.urlopen(news_url)
+
+parseResponse = json.loads(response.read())
+
+print(parseResponse)
